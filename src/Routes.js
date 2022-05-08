053e329498b79ced64b412926fd52b1195c8d255
sabin <sashre@utu.fi>
2022-05-08 18:58:53 +0300

    header removed

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,12 @@
 import React from "react";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/header";
+import Navbar from "./components/navbar";
 import Home from "./pages/home";
 
 export const Routes = () => {
   return (
     <Router>
-      <Header />
+      <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
